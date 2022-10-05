0950c544ca671d8ea50fa24bcf49f045ba36939d
enbytedev <me@enbyte.dev>
2022-10-04 23:52:04 -0700

    Fixed issue with bad input.

--- src/scripts/stdout/processing.js
+++ src/scripts/stdout/processing.js
@@ -4,12 +4,12 @@ import {template} from 'chalk-template';
 /**
  * Process the console log to stdout.
  * @param {string} message 
- * @param {string | undefined} context 
+ * @param {string} context 
  * @param {object} format 
  */
 export function processConsoleLog(message, context, format) {
     let messageLines = message.match(/[^\r\n]+/g);
-    if (context !== undefined) { context = context.toUpperCase() + ' > '; } else { context = ''; }
+    if (typeof context === 'string' && context != '') { context = context.toUpperCase() + ' > '; } else { context = ''; }
 
     messageLines.forEach((line) => {
         process.stdout.write(template(format(moment, context, line)));
